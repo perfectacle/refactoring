81474ad418a84692193e626d7066362f8a35cccd
perfectacle <perfectacle@gmail.com>
2018-06-20 12:16:44 +0900

    10th refactoring - getCharge method makes abstract method

--- src/main/com/company/ChildrensPrice.java
+++ src/main/com/company/ChildrensPrice.java
@@ -5,4 +5,12 @@ public class ChildrensPrice implements Price {
     public int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if(daysRented > 3) result += (daysRented - 3) * 1.5;
+
+        return result;
+    }
 }
--- src/main/com/company/Price.java
+++ src/main/com/company/Price.java
@@ -2,28 +2,5 @@ package com.company;
 
 public interface Price {
     int getPriceCode();
-    default double getCharge(int daysRented) {
-        double result = 0;
-
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-
-                if(daysRented > 2) result += (daysRented - 2) * 1.5;
-
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-
-                if(daysRented > 3) result += (daysRented - 3) * 1.5;
-
-                break;
-        }
-
-        return result;
-    };
+    double getCharge(int daysRented);
 }
