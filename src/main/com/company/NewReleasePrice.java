6ecfd3d65ddebc1da7e5d6fec63614c4ce71dd6b
perfectacle <perfectacle@gmail.com>
2018-06-20 09:23:46 +0900

    seventh refactoring - extract method for movie's charge and points

---
+++ src/main/com/company/NewReleasePrice.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class RegualrPrice implements Price {
+    public int getPriceCode() {
+        return Movie.REGULAR;
+    }
+}
---
+++ src/main/com/company/RegualrPrice.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class ChildrensPrice implements Price {
+    public int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+}
